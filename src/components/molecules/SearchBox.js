7db4b83cdd9680909c04df9e042e66f526f9b5ee
dv-jamie <sjm2506@gmail.com>
2023-01-07 03:45:53 +0900

    searchBox css 업데이트

--- src/components/molecules/SearchBox.js
+++ src/components/molecules/SearchBox.js
@@ -4,11 +4,13 @@ import searchIcon from "../../assets/icons/searchIcon.png"
 function SearchBox() {
     return (
         <div className={styles.container}>
-            <input
-                placeholder="원하는 레시피를 검색해보세요"
-                spellcheck="false"
-            />
-            <img src={searchIcon} alt="검색 아이콘" />
+            <div className={styles.search_wrap}>
+                <input
+                    placeholder="원하는 레시피를 검색해보세요"
+                    spellcheck="false"
+                />
+                <img src={searchIcon} alt="검색 아이콘" />
+            </div>
         </div>
     );
 }
