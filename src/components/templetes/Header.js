415b25c7b638ea2c99a6328c99f7f6f4f3fa5289
dv-jamie <sjm2506@gmail.com>
2023-01-07 02:07:15 +0900

    헤더 레이아웃 작업

--- src/components/templetes/Header.js
+++ src/components/templetes/Header.js
@@ -1,7 +1,18 @@
+import styles from "./Header.module.css";
+import bookmarkIcon from "../../assets/icons/bookmarkIcon.png"
+import profileIcon from "../../assets/icons/profileIcon.png"
+
 function Header() {
     return (
-        <div>
-            헤더
+        <div className={styles.container}>
+            <ul className={styles.nav}>
+                <li>
+                    <img src={bookmarkIcon} alt="북마크 아이콘" />
+                </li>
+                <li>
+                    <img src={profileIcon} alt="프로필 아이콘" />
+                </li>
+            </ul>
         </div>
     );
 }
