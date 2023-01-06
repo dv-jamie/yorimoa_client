246b7f8c3500cb70f2f269bbc76da85785cc1611
dv-jamie <sjm2506@gmail.com>
2023-01-07 02:06:39 +0900

    전체 레이아웃 작업

--- src/components/templetes/Layout.js
+++ src/components/templetes/Layout.js
@@ -1,15 +1,20 @@
 import { Outlet } from "react-router-dom";
 import Header from "./Header";
 import Nav from "./Nav";
+import styles from "./Layout.module.css";
 
 function Layout() {
     return (
-        <div>
-            <Header />
-            <div>
+        <div className={styles.container}>
+            <header className={styles.header_area}>
+                <Header />
+            </header>
+            <main className={styles.main_area}>
                 <Outlet />
-            </div>
-            <Nav />
+            </main>
+            <footer className={styles.footer_area}>
+                <Nav />
+            </footer>
         </div>
     );
 }
