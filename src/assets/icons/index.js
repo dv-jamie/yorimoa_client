c92b82ef5fc4acc1c700c21da496129744a53c23
dv-jamie <sjm2506@gmail.com>
2023-02-11 22:35:39 +0900

    냉장고 관련 아이콘, 이미지 추가

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -1,18 +1,25 @@
 import { ReactComponent as ArrowDownOutline } from './arrowDownOutline.svg'
 import { ReactComponent as BackOutline } from './backOutline.svg'
+import { ReactComponent as BagFill } from './bagFill.svg'
 import { ReactComponent as BookmarkOutline } from './bookmarkOutline.svg'
 import { ReactComponent as CategoryOutline } from './categoryOutline.svg'
 import { ReactComponent as CheckboxFill } from './checkboxFill.svg'
 import { ReactComponent as ClockOutline } from './clockOutline.svg'
+import { ReactComponent as CloseOutline } from './closeOutline.svg'
 import { ReactComponent as CommentOutline } from './commentOutline.svg'
 import { ReactComponent as DiaryFill } from './diaryFill.svg'
+import { ReactComponent as KakaoSymbol } from './kakaoSymbol.svg'
 import { ReactComponent as LevelOutline } from './levelOutline.svg'
 import { ReactComponent as MemoFill } from './memoFill.svg'
+import { ReactComponent as PencilOutline } from './pencilOutline.svg'
 import { ReactComponent as PencilFill } from './pencilFill.svg'
 import { ReactComponent as PersonOutline } from './personOutline.svg'
 import { ReactComponent as RecipeOutline } from './recipeOutline.svg'
+import { ReactComponent as RefrigeratorFill } from './refrigeratorFill.svg'
 import { ReactComponent as SearchOutline } from './searchOutline.svg'
 import { ReactComponent as ServingOutline } from './servingOutline.svg'
+import { ReactComponent as SettingOutline } from './settingOutline.svg'
+import { ReactComponent as SpoonFill } from './spoonFill.svg'
 import { ReactComponent as ToqueFill } from './toqueFill.svg'
 import { ReactComponent as ToqueOutline } from './toqueOutline.svg'
 
@@ -32,19 +39,26 @@ import { ReactComponent as WithAlcohol } from './theme/withAlcohol.svg'
 export {
     ArrowDownOutline,
     BackOutline,
+    BagFill,
     BookmarkOutline,
     CategoryOutline,
     CheckboxFill,
     ClockOutline,
+    CloseOutline,
     CommentOutline,
     DiaryFill,
+    KakaoSymbol,
     LevelOutline,
     MemoFill,
+    PencilOutline,
     PencilFill,
     PersonOutline,
     RecipeOutline,
+    RefrigeratorFill,
     SearchOutline,
     ServingOutline,
+    SettingOutline,
+    SpoonFill,
     ToqueFill,
     ToqueOutline,
  
