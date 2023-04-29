f112f3e34f8512aef4a2da2ee6b81abca187f525
dv-jamie <sjm2506@gmail.com>
2023-04-29 15:26:15 +0900

    환경변수 설정

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import axios from "axios";
-import Layout from './pages/Layout';
+import Layout from "./pages/Layout";
 import Refrigerator from "pages/Refrigerator";
 import ComingSoon from "pages/ComingSoon";
 import Login from "pages/Login";
@@ -11,64 +11,106 @@ import RefrigeratorEdit from "pages/RefrigeratorEdit";
 import KakaoAuthHandle from "_auth/KakaoAuthHandle";
 
 function App() {
-  const HOME_PAGE_PATH = "/refrigerator"
-  const LOGIN_PAGE_PATH = "/login"
+    const HOME_PAGE_PATH = "/refrigerator";
+    const LOGIN_PAGE_PATH = "/login";
 
-  const [isLogin, setIsLogin] = useState(false)
-  const [isModalShow, setIsModalShow] = useState(false)
-  const [bottomsheetType, setBottomsheetType] = useState("")
-  const [isBottomsheetShow, setIsBottomsheetShow] = useState(false)
-  const [isTwoDepthBottomsheetShow, setIsTwoDepthBottomsheetShow] = useState(false)
+    const [isLogin, setIsLogin] = useState(false);
+    const [isModalShow, setIsModalShow] = useState(false);
+    const [bottomsheetType, setBottomsheetType] = useState("");
+    const [isBottomsheetShow, setIsBottomsheetShow] = useState(false);
+    const [isTwoDepthBottomsheetShow, setIsTwoDepthBottomsheetShow] =
+        useState(false);
 
-  useEffect(() => {
-    const jwtToken = localStorage.getItem("JWT_TOKEN")
-    
-    if(jwtToken) {
-      axios.defaults.headers.common['Authorization'] = `Bearer ${jwtToken}`
-      setIsLogin(true)
-    }
-  }, [])
+    useEffect(() => {
+        const jwtToken = localStorage.getItem("JWT_TOKEN");
 
-  return (
-    <>
-      <BrowserRouter basename={process.env.PUBLIC_URL}>
-        <Routes>
-          <Route element={<Layout
-            isLogin={isLogin}
-            setIsLogin={setIsLogin}
-            isModalShow={isModalShow}
-            setIsModalShow={setIsModalShow}
-            bottomsheetType={bottomsheetType}
-            setBottomsheetType={setBottomsheetType}
-            isBottomsheetShow={isBottomsheetShow}
-            setIsBottomsheetShow={setIsBottomsheetShow}
-            isTwoDepthBottomsheetShow={isTwoDepthBottomsheetShow}
-            setIsTwoDepthBottomsheetShow={setIsTwoDepthBottomsheetShow}
-          />}>
-            <Route path="/auth/kakao" element={<KakaoAuthHandle setIsLogin={setIsLogin} />}></Route>
-            
-            <Route path="/" element={
-              <Navigate to={ isLogin ? HOME_PAGE_PATH : LOGIN_PAGE_PATH } />
-            }></Route>
-            <Route path="/login" element={
-              isLogin ? <Navigate to="/" /> : <Login />
-            }></Route>
-            <Route path="/setting" element={<Setting />}></Route>
-            <Route path="/quit" element={<Quit />}></Route>
+        if (jwtToken) {
+            axios.defaults.headers.common[
+                "Authorization"
+            ] = `Bearer ${jwtToken}`;
+            setIsLogin(true);
+        }
+    }, []);
 
-            <Route path="/diary" element={<ComingSoon type="diary" />}></Route>
-            <Route path="/recipe" element={<ComingSoon type="recipe" />}></Route>
-            <Route path="/refrigerator" element={
-              isLogin ? <Refrigerator /> : <Navigate to={LOGIN_PAGE_PATH} />
-            }></Route>
-            <Route path="/refrigerator/edit" element={<RefrigeratorEdit />}></Route>
-            
-            <Route path="*" element={<Navigate to="/" />}></Route>
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </>
-  );
+    return (
+        <>
+            <BrowserRouter basename={process.env.PUBLIC_URL}>
+                <Routes>
+                    <Route
+                        element={
+                            <Layout
+                                isLogin={isLogin}
+                                setIsLogin={setIsLogin}
+                                isModalShow={isModalShow}
+                                setIsModalShow={setIsModalShow}
+                                bottomsheetType={bottomsheetType}
+                                setBottomsheetType={setBottomsheetType}
+                                isBottomsheetShow={isBottomsheetShow}
+                                setIsBottomsheetShow={setIsBottomsheetShow}
+                                isTwoDepthBottomsheetShow={
+                                    isTwoDepthBottomsheetShow
+                                }
+                                setIsTwoDepthBottomsheetShow={
+                                    setIsTwoDepthBottomsheetShow
+                                }
+                            />
+                        }
+                    >
+                        <Route
+                            path="/auth/kakao"
+                            element={
+                                <KakaoAuthHandle setIsLogin={setIsLogin} />
+                            }
+                        ></Route>
+
+                        <Route
+                            path="/"
+                            element={
+                                <Navigate
+                                    to={
+                                        isLogin
+                                            ? HOME_PAGE_PATH
+                                            : LOGIN_PAGE_PATH
+                                    }
+                                />
+                            }
+                        ></Route>
+                        <Route
+                            path="/login"
+                            element={isLogin ? <Navigate to="/" /> : <Login />}
+                        ></Route>
+                        <Route path="/setting" element={<Setting />}></Route>
+                        <Route path="/quit" element={<Quit />}></Route>
+
+                        <Route
+                            path="/diary"
+                            element={<ComingSoon type="diary" />}
+                        ></Route>
+                        <Route
+                            path="/recipe"
+                            element={<ComingSoon type="recipe" />}
+                        ></Route>
+                        <Route
+                            path="/refrigerator"
+                            element={
+                                isLogin ? (
+                                    <Refrigerator />
+                                ) : (
+                                    <Navigate to={LOGIN_PAGE_PATH} />
+                                )
+                            }
+                        ></Route>
+                        <Route
+                            path="/refrigerator/edit"
+                            element={<RefrigeratorEdit />}
+                        ></Route>
+
+                        <Route path="*" element={<Navigate to="/" />}></Route>
+                    </Route>
+                </Routes>
+            </BrowserRouter>
+        </>
+    );
 }
 
 export default App;
